3d24f71c934e185036c368ca0e3418bb4c2f3bf3
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-04-25 18:07:54 +0000

    vercel analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,26 @@ import WishlistPage from './pages/WishlistPage';
 import CheckoutPage from './pages/CheckoutPage';
 import ProfilePage from './pages/ProfilePage';
 
+import { Analytics } from "@vercel/analytics/react"
+
 function App() {
   return (
-    <Routes>
-      <Route path="/computers" element={<ComputersPage/>}/>
-      <Route path="/:category/:id" element={<ProductPage/>}/>
-      <Route path="/phone" element={<PhonePage/>}/>
-      <Route path="/smartwatch" element={<SmartWatchesPage/>}/>
-      <Route path="/gaming" element={<GamingPage/>}/>
-      <Route path="/head-phones" element={<HeadPhonesPage/>}/>
-      <Route path="/cart" element={<CartPage/>}/>
-      <Route path="/checkout" element={<CheckoutPage/>}/>
-      <Route path="/favorite" element={<WishlistPage/>}/>
-      <Route path="/profile" element={<ProfilePage/>}/>
-      <Route index element={<Home/>}/>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/computers" element={<ComputersPage/>}/>
+        <Route path="/:category/:id" element={<ProductPage/>}/>
+        <Route path="/phone" element={<PhonePage/>}/>
+        <Route path="/smartwatch" element={<SmartWatchesPage/>}/>
+        <Route path="/gaming" element={<GamingPage/>}/>
+        <Route path="/head-phones" element={<HeadPhonesPage/>}/>
+        <Route path="/cart" element={<CartPage/>}/>
+        <Route path="/checkout" element={<CheckoutPage/>}/>
+        <Route path="/favorite" element={<WishlistPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+        <Route index element={<Home/>}/>
+      </Routes>
+      <Analytics/>
+    </>
   )
 }
 
